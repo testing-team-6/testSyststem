174e9004c2e1f3eecea7a10d0c0820fb12997a2e
fd17302010041 <17302010041@fudan.edu.cn>
2020-04-11 11:13:11 +0800

    完善了ExportPaperAction.java中的 generatePaperPdf方法

--- sourceCode/test-maker/src/main/java/cn/cstqb/exam/testmaker/actions/paper/ExportPaperAction.java
+++ sourceCode/test-maker/src/main/java/cn/cstqb/exam/testmaker/actions/paper/ExportPaperAction.java
@@ -1,13 +1,24 @@
 package cn.cstqb.exam.testmaker.actions.paper;
 
+import cn.cstqb.exam.testmaker.dao.PaperDao;
+import cn.cstqb.exam.testmaker.dao.impl.PaperDaoImpl;
 import cn.cstqb.exam.testmaker.entities.Paper;
+import cn.cstqb.exam.testmaker.entities.Question;
+import cn.cstqb.exam.testmaker.entities.QuestionChoice;
+import cn.cstqb.exam.testmaker.util.PDFUtils;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -40,8 +51,67 @@ public class ExportPaperAction extends BasePaperAction {
     }
 
     private void generatePaperPdf(Integer paperId) {
-        System.out.println(paperService == null);
-        Paper paper = paperService.find(paperId);
-        System.out.println(paper.getName());
+        try {
+
+            // 1.新建document对象
+            Document document = new Document();
+
+            // 2.建立一个书写器(Writer)与document对象关联，通过书写器(Writer)可以将文档写入到磁盘中。
+            // 创建 PdfWriter 对象 第一个参数是对文档对象的引用，第二个参数是文件的实际名称，在该名称中还会给出其输出路径。
+            File file = new File("./pdf/test.pdf");
+            if(file.exists()){//删除以往的PDF文件
+                file.delete();
+            }
+            File fileParent = file.getParentFile();
+            if(!fileParent.exists()){
+                fileParent.mkdirs();
+            }
+            file.createNewFile(); //创建新的PDF
+            PdfWriter.getInstance(document, new FileOutputStream(file));
+
+            // 3.打开文档
+            document.open();
+
+            // 中文字体
+            BaseFont bfChinese = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+            Font font = new Font(bfChinese);
+
+            // 4.添加内容
+            Paper paper = paperService.find(paperId);
+            document.add(new Phrase(paper.getName() + "\n",font)); //添加试卷名称
+
+            List<Question> questionList = paper.getQuestions();
+            int count = 1; //题号
+            for (Question q:questionList
+            ) {
+                document.add(new Phrase(count + ". " + q.getType().getName() + "  (" + q.getScore() + "分)", font));
+                document.add(new Paragraph(q.getStem(),font));//题干
+                document.add(new Paragraph(q.getScenario(),font));//情景
+
+                Set<QuestionChoice> questionChoices = q.getChoices();//选项
+                if(questionChoices.size() > 0){
+                    if(q.isMultipleChoice()){
+                        document.add(new Paragraph("(多选题）",font));
+                    }else {
+                        document.add(new Paragraph("(单选题）",font));
+                    }
+
+                    for (QuestionChoice choice :
+                            questionChoices) {
+                        String str = choice.getChoiceLabel() + ". " + choice.getContent();
+                        document.add(new Paragraph(str, font));
+                    }
+                }
+
+                document.add(new Paragraph("\n", font));
+                count ++;
+            }
+
+            // 5.关闭文档
+            document.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
